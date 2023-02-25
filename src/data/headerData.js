4df9558f033091f2f22c0af786482ca126bdcc03
Asad Ishfaq <asadishfaq@aquila360.com>
2023-02-26 03:35:42 +0500

    add images

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,10 @@
 import resume from "../assets/pdf/resume.pdf";
-
+import asad from "../assets/jpeg/asad6.jpeg";
 export const headerData = {
   name: "Muhammad Asad",
   title: "Full Stack Developer",
   desciption:
     "Life may be a meandering path but don't worry about it, rather enjoy it. It is about the journey, not the destination so explore all that comes your way, learn and grow from it, for this journey only happens once.",
-  image: "https://avatars.githubusercontent.com/u/85482640?v=4",
+  image: asad,
   resumePdf: resume,
 };
